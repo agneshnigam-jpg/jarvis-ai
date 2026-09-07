a0da351347e57a457d332ec893ecfca0e0fe612e
agneshnigam-jpg <agneshnigam@gmail.com>
2026-09-07 20:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def chat():
 
     try:
         response = client.models.generate_content(
-            model="gemini-3.6-flash",
+            model="gemini-flash-lite-latest",
             contents=user_message
         )
         return jsonify({"reply": response.text})
